d749b741e205979d316d588c38403a4ca96304e3
matecsornyei <mate@csornyei.com>
2021-03-27 01:04:46 +0100

    Stack implemented

--- stack.js
+++ stack.js
@@ -1,1 +1,39 @@
 // Your implementation goes here
+class Stack {
+    constructor() {
+        this.items = Array.from(arguments);
+    }
+
+    get size() {
+        return this.items.length
+    }
+
+    push(item) {
+        this.items.push(item);
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    pop() {
+        return this.items.pop() || null;
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    swap() {
+        if (this.size > 1) {
+            const last = this.items[this.items.length - 1];
+            const beforeLast = this.items[this.items.length - 2];
+            this.items[this.items.length - 1] = beforeLast;
+            this.items[this.items.length - 2] = last;
+        } else {
+            throw new Error('Not enough element to swap!');
+        }
+    }
+}
+
+module.exports = Stack;
